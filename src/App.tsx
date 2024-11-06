6bfdf42015369531750273dfe8ed4d7997f82642
PEDRO SAMUEL SOARES SIMÃO <pedro.simao_estudant@C135756.pdc-academico.ifms.tl>
2024-11-05 20:30:15 -0400

    add: New styles in TodoItem and create alert to remove item and checked itens finisheds

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,115 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Check, RotateCcw, Trash2Icon, Undo } from "lucide-react"
 
-function App() {
-  const [number, setNumber] = useState<number>(0)
+interface Item {
+  name: string;
+  completed: boolean; 
+}
 
+function Menu() {
+  return (
+    <div className="py-2 border-b">
+      Lista de Tarefas
+    </div>
+  );
+}
 
-  useEffect(() => {
-    setTimeout(()=> {
-      setNumber(number+1);
-    }, 1100)
-  }, [number]) 
+function TodoItem({ item, concluir, remover }: {item: Item, concluir: any, remover: any}) {
+  return (
+    <div className={`flex items-center justify-between p-2 rounded-md border bg-slate-50 transition-all shadow-sm hover:shadow-md ${
+      item.completed && 'opacity-45 transition-all hover:opacity-80 '
+    }`}>
+      <div className={item.completed ? 'line-through' : ''}>{item.name}</div>
+      <div className="flex gap-3">
+
+        <Button variant="outline" size="icon" className={`text-green-500 hover:text-green-600`} onClick={concluir}>
+          {
+            !item.completed ? (
+              <Check className="h-4 w-4" />
+            ) : <Undo className="h-4 w-4 text-gray-500 hover:text-gray-700" />
+          }
+        </Button>
+        <Button variant="outline" size="icon"  className={`text-red-500 hover:text-red-600`} onClick={remover}>
+          <Trash2Icon className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function Conteudo() {
+  const [itens, setItens] = useState([] as Item[]);
+
+  function submeterFormulario(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    
+    const f = e.target as HTMLElement;
+    const input = f.querySelector('input') as HTMLInputElement;
+    const titulo = input.value;
+    if (titulo) {
+
+      const item: Item = {
+        name: titulo,
+        completed: false,
+      }
+
+      setItens([...itens, item]);
+      input.value = '';
+    } 
+  }
+
+  function remover(index: number) {
+    const value = confirm('Deseja realmente excluir?')
+    if(value) {
+
+      setItens(itens.filter((_, i) => i !== index));
+    }
+  }
+
+  function concluir(index: number) {
+    const updatedItens = [...itens];
+    const item = updatedItens[index]; 
+    
+    item.completed = !item.completed;
+    setItens(updatedItens)
+  }
 
   return (
-    <>
-      <div className="bg-slate-400 h-screen flex justify-center items-center selection:bg-none" >
-        <p className="text-8xl font-semibold text-slate-200 drop-shadow-xl animate-bounce ">{number}</p>
+    <div className="space-y-5">
+      <form onSubmit={(e) => submeterFormulario(e)}>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="text" placeholder="Escreva uma tarefa..." />
+          <Button type="submit">Adicionar</Button>
+        </div>
+      </form>
+
+      <div className="space-y-2">
+        {itens.map((el, index) => {
+          return <TodoItem key={index} item={el} concluir={() => concluir(index)} remover={() => remover(index)} />
+        })}
       </div>
-    </>
-  )
+    </div>
+  );
+}
+
+function Rodape() {
+  return (
+    <div className="py-2 border-t">
+      TADS &copy; 2024
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="m-4 space-y-6">
+      <Menu />
+      <Conteudo />
+      <Rodape />
+    </div>
+  );
 }
 
 export default App
